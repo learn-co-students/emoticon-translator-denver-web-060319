f376a8fb41cbc7ca5e0759489773025dfcb1b4fa
Rj <rikrdo.ortega@gmail.com>
2019-06-08 16:54:19 -0600

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,38 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+# {"angel"=>["O:)", "☜(⌒▽⌒)☞"],
+#  "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
+#  "bored"=>[":O", "(ΘεΘ;)"],
+#  "confused"=>["%)", "(゜.゜)"],
+#  "embarrased"=>[":$", "(#^.^#)"],
+#  "fish"=>["><>", ">゜))))彡"],
+#  "glasses"=>["8D", "(^0_0^)"],
+#  "grinning"=>["=D", "(￣ー￣)"],
+#  "happy"=>[":)", "(＾ｖ＾)"],
+#  "kiss"=>[":*", "(*^3^)/~☆"],
+#  "sad"=>[":'(", "(Ｔ▽Ｔ)"],
+#  "surprised"=>[":o", "o_O"],
+#  "wink"=>[";)", "(^_-)"]}
+
+def load_library path
+  file_library = YAML.load_file(path)
+  library = file_library.each_with_object ({}) do |(emotion,emoticons), hash|
+    hash["get_meaning"] ||= {}
+    hash["get_emoticon"] ||= {}
+    hash["get_meaning"][emoticons[1]] = emotion
+    hash["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+# load_library './lib/emoticons.yml'
+
+def get_japanese_emoticon path, emoticon
+  library = load_library(path)
+  library["get_emoticon"].fetch(emoticon, "Sorry, that emoticon was not found")
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning path, emoticon
+  library = load_library(path)
+  library["get_meaning"].fetch(emoticon, "Sorry, that emoticon was not found")
 end
